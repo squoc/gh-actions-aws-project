19c5974f9037060b721a431035be69c86cfc0fea
Quoc Nguyen <squoc.nguyen@gmail.com>
2025-04-09 15:10:29 +0700

    add instance

--- main.tf
+++ main.tf
@@ -3,3 +3,28 @@ resource "aws_ssm_parameter" "my_param" {
   type  = "String"
   value = var.param_value
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "hello_world" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
